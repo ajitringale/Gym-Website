936f7ff6cfd0cb2075112c582eed9aded1c3bae5
ajitringale <157112236+ajitringale@users.noreply.github.com>
2024-02-01 16:00:15 +0530

    Add files via upload

---
+++ fit.js
@@ -0,0 +1,71 @@
+let card = document.querySelectorAll(".slider");
+let challenge = document.querySelectorAll(".card");
+let connect = document.getElementById("connect");
+
+
+let count = 0;
+
+card.forEach(function(cards, index) {
+    cards.style.left = `${index*100}%`
+})
+
+function myFun() {
+    card.forEach(function(curVal) {
+        curVal.style.transform = `translateX(-${100*count}%)`
+    })
+}
+
+setInterval(function() {
+    count++;
+    if(count == card.length) {
+        count = 0;
+    }
+    myFun();
+}, 2000)
+
+// card Detail
+challenge.forEach(function(detail) {
+    detail.addEventListener("click", function() {
+        document.querySelector(".container").innerHTML="";
+
+        let div = document.createElement("div");
+        div.classList.add("detailCard");
+        div.innerHTML= `
+        <h2>Challenges</h2>
+            <p>12 Weeks</p>
+            <img src=${detail.firstElementChild.src} alt="">
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique hic exercitationem dolores porro nulla. Quae repellendus id iste reprehenderit nihil ipsa dicta dolore dolorem?</p>
+            <button>Join</button>
+            <a href="">Back</a>
+            `
+
+            document.querySelector("body").appendChild(div)
+                console.log(detail.firstElementChild);
+    })
+})
+
+
+connect.addEventListener("click", function() {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    if(name.value == "" || email.value == "" || pass.value == "") {
+        alert("Please Enter Details")
+    }
+    else {
+        alert("THANKS FOR CONNECTING.")
+    }
+});
+
+// password eye icon
+const togglePassword = document.querySelector('#eye');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function(e) {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    this.classList.toggle('fa-eye');
+});
